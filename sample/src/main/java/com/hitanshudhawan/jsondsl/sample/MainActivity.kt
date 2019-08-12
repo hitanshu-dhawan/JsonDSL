4f8932c008a9975fb223a26ad85d329e653a304e
Hitanshu Dhawan <hitanshudhawan1996@gmail.com>
2019-08-13 01:21:03 +0530

    JsonDSL

--- sample/src/main/java/com/hitanshudhawan/jsondsl/sample/MainActivity.kt
+++ sample/src/main/java/com/hitanshudhawan/jsondsl/sample/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.hitanshudhawan.jsondsl.sample
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.hitanshudhawan.jsondsl.json
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView.text = json {
+            "name" to "Hitanshu"
+            "age" to 22
+            "male" to true
+        }.toString(2)
     }
 }
